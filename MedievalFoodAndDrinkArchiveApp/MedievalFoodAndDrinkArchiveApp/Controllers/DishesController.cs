fd1bd2bcc2cf4df059b4fc413eb3ae5bc69a3ce5
Fei Roth <fei.roth@uhr.se>
2019-03-10 13:32:51 +0100

    Added GetDishes HttpGet endpoint and new Dish dummy data.

---
+++ MedievalFoodAndDrinkArchiveApp/MedievalFoodAndDrinkArchiveApp/Controllers/DishesController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MedievalFoodAndDrinkArchiveApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MedievalFoodAndDrinkArchiveApp.Controllers
+{
+    [Route("api/[controller]")]
+    public class DishesController : Controller
+    {
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        [HttpGet]
+        public IEnumerable<Dish> GetDishes()
+        {
+            return new[]
+            {
+                new Dish
+                {
+                    Name = "Truffle salad with garlic",
+                    Description = "Aphrodisiacs",
+                    Id = 1,
+                    Price = 13.99
+                }
+            };
+        }
+    }
+}
