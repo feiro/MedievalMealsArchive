84496015e86f21c7c303791c855d1348f561ea08
Fei Roth <fei.roth@uhr.se>
2019-03-10 15:55:39 +0100

    Added scaffolding for dish repository interface and class.

---
+++ MedievalFoodAndDrinkArchiveApp/MedievalFoodAndDrinkArchiveApp/Repositories/FileDishRepository.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MedievalFoodAndDrinkArchiveApp.Models;
+
+namespace MedievalFoodAndDrinkArchiveApp.Repositories
+{
+    public class FileDishRepository : IDishRepository
+    {
+        public IEnumerable<Dish> GetDishes()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Dish GetDishById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Dish CreateDish(Dish dish)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Dish UpdateDish(Dish dish)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteDish(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MedievalFoodAndDrinkArchiveApp/MedievalFoodAndDrinkArchiveApp/Repositories/IDishRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MedievalFoodAndDrinkArchiveApp.Models;
+
+namespace MedievalFoodAndDrinkArchiveApp.Repositories
+{
+    public interface IDishRepository
+    {
+        // Return a complete list of all available dishes.
+        IEnumerable<Dish> GetDishes();
+        // Return a specific dish.
+        Dish GetDishById(int id);
+        // Create/add a new dish.
+        Dish CreateDish(Dish dish);
+        // Update information on existing dish.
+        Dish UpdateDish(Dish dish);
+        // Delete specific dish.
+        void DeleteDish(int id);
+    }
+}
