05a296d69e4d7cb0f41c6c21541a17ddf3f7fc28
Fei Roth <fei.roth@uhr.se>
2019-03-10 21:02:59 +0100

    Added Context class och ConnectionStrings values.

---
+++ MedievalFoodAndDrinkArchiveApp/MedievalFoodAndDrinkArchiveApp/Models/MedievalMealsArchiveContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace MedievalFoodAndDrinkArchiveApp.Models
+{
+    public class MedievalMealsArchiveContext : DbContext
+    {
+        public MedievalMealsArchiveContext(DbContextOptions<MedievalMealsArchiveContext> options):base(options)
+        {
+            
+        }
+        public DbSet<Dish> Dishes { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
