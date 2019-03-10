998ef8f31eb80ef7ae84e5b70842835049c5efaf
Fei Roth <fei.roth@uhr.se>
2019-03-10 21:10:59 +0100

    Updated Startup with AddDbContext information.

--- MedievalFoodAndDrinkArchiveApp/MedievalFoodAndDrinkArchiveApp/Startup.cs
+++ MedievalFoodAndDrinkArchiveApp/MedievalFoodAndDrinkArchiveApp/Startup.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MedievalFoodAndDrinkArchiveApp.Models;
 using MedievalFoodAndDrinkArchiveApp.Repositories;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -30,6 +32,8 @@ namespace MedievalFoodAndDrinkArchiveApp
             services.AddScoped<IDishRepository, FileDishRepository>();
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddDbContext<MedievalMealsArchiveContext>(
+                options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
