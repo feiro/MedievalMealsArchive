a54aa2ab4c4f5bc360483e642285379de360170b
Fei Roth <fei.roth@uhr.se>
2019-03-11 10:26:16 +0100

    Encapsulation for dish create and delete.

---
+++ MedievalFoodAndDrinkArchiveApp/MedievalFoodAndDrinkArchiveApp/Repositories/EfDishRepository.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MedievalFoodAndDrinkArchiveApp.Models;
+
+namespace MedievalFoodAndDrinkArchiveApp.Repositories
+{
+    public class EfDishRepository : IDishRepository
+    {
+        // EF context
+        private readonly MedievalMealsArchiveContext _db;
+        // Handle through dependency injection
+        public EfDishRepository(MedievalMealsArchiveContext db)
+        {
+            _db = db;
+        }
+         
+        public IEnumerable<Dish> GetDishes()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Dish GetDishById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Dish CreateDish(Dish dish)
+        {
+            _db.Dishes.Add(dish);
+            _db.SaveChanges();
+            return dish;
+        }
+
+        public Dish UpdateDish(Dish dish)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteDish(int id)
+        {
+            var dish = _db.Dishes.Find(id);
+            if (dish != null)
+            {
+                _db.Dishes.Remove(dish);
+                _db.SaveChanges();
+            }
+        }
+    }
+}
